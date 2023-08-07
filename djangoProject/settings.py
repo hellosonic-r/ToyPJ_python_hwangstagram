3786f27314d6aaa065f5550e2b023988bbb8e27e
hellosonic-r <hellommmh@gmail.com>
2023-08-07 21:47:05 +0900

    수정

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
-        'NAME': 'hwangstagram',
-        'USER': os.environ.get('MYSQL_USER_NAME', 'default_user'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'default_password'),
-        'PORT': '3306',
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+#         'NAME': 'hwangstagram',
+#         'USER': os.environ.get('MYSQL_USER_NAME', 'default_user'),
+#         'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'default_password'),
+#         'PORT': '3306',
+#         'OPTIONS': {'charset': 'utf8mb4'},
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
